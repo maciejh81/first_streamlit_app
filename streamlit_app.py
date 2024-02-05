23af18b08d7978033185c59512622748241968d3
Maciej Hoffmann <122646866+maciejh81@users.noreply.github.com>
2024-02-05 10:58:59 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,8 +33,10 @@ def get_fruityvice_data(this_fruit_choice):
 
 #new section to display fruityvice api response
 back_from_function = get_fruityvice_data(fruit_choice)
+
 streamlit.dataframe(back_from_function)
 streamlit.header("Fruityvice Fruit Advice!")
+
 try:
     fruit_choice = streamlit.text_input('What fruit would you like information about?')
     if not fruit_choice:

285c2e09da9ca6b7e462c30dfec6bb7e6925c314
Maciej Hoffmann <122646866+maciejh81@users.noreply.github.com>
2024-02-05 10:25:49 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -35,6 +36,8 @@ streamlit.write('The user entered ', fruit_choice)
 
 # show normalized table
 streamlit.dataframe(fruityvice_normalized)
+
+streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
